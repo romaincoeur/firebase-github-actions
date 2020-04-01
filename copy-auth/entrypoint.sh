dfcd27a638bfe5e8d58a081471b12b4d6a20901f
Romain Coeur <coeurrogmail.com>
2020-04-01 11:33:59 +0200

    copy auth

---
+++ copy-auth/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+if [ -z "$FIREBASE_TOKEN" ]; then
+  echo "FIREBASE_TOKEN is required to run commands with the firebase cli"
+  exit 126
+fi
+
+firebase use --add "$1"
+firebase auth:export users.json
+firebase use --add "$2"
+firebase auth:import users.json --hash-algo=SCRYPT --rounds=8 --mem-cost=14 --hash-key=["$BASE64_SIGNER_KEY"]
+rm users.json
