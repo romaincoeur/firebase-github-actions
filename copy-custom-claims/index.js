6f74d0a1926e4c808ca6bd7f9ff060f0aa51dc30
Romain <coeurro@gmail.com>
2020-12-09 19:44:38 +0100

    fix Exceeded quota error

--- copy-custom-claims/index.js
+++ copy-custom-claims/index.js
@@ -10,7 +10,23 @@ var to = admin.initializeApp({
 
 const migrateAllUsers = async (nextPageToken, count=0) => {
   const listUsersResult = await from.auth().listUsers(1000, nextPageToken)
+  console.log(`Moving ${listUsersResult.users.length} users`)
+  const retry = []
   for (const userRecord of listUsersResult.users) {
+    try {
+      await to.auth().setCustomUserClaims(userRecord.uid, userRecord.customClaims)
+      count++
+      !process.env.CI && process.stdout.write(`${count}\r`)
+    }
+    catch(error) {
+      if (userRecord.customClaims) {// Ignore error for Anonymous users without custom claims
+        console.error('Error writing custom claims for user: ' + userRecord.uid + ' - Error: ' + error.toString())
+        retry.push(userRecord)
+      }
+    }
+  }
+  console.log(`Retry ${retry.length} users`)
+  for (const userRecord of retry) {
     try {
       await to.auth().setCustomUserClaims(userRecord.uid, userRecord.customClaims)
       count++
